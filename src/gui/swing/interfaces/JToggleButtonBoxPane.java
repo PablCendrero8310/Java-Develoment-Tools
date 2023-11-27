ac46c95e81211207d33a8d063e532c4ff0ce8ec9
PablCendrero8310 <pablolinux@pablolinux-VivoBook-ASUSLaptop-X415EA-F1400EA>
2023-11-27 16:46:11 +0100

    Add javadoc

--- src/gui/swing/interfaces/JToggleButtonBoxPane.java
+++ src/gui/swing/interfaces/JToggleButtonBoxPane.java
@@ -3,8 +3,8 @@ package gui.swing.interfaces;
 import javax.swing.*;
 /**
  * 
- * @param <T>
- * @param <R>
+ * @param <T> Type of JToggleButton of the Box
+ * @param <R> The return type of the method <code>getSelection</code>
  */
 public interface JToggleButtonBoxPane<T extends JToggleButton, R> {
 	R getSelection();
